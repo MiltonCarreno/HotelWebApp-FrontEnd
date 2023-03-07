d8df8f4c4e73c08c590a78d9f22abb2328c0936e
MiltonCarreno <miltonyaredcr@gmail.com>
2023-03-06 22:54:22 -0500

    Updated axios url and cleaned/removed code

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://b15d-97-104-231-134.ngrok.io",
+    baseURL: "https://58a8-108-188-68-70.ngrok.io",
     headers: {
         "ngrok-skip-browser-warning": "true",
         "Access-Control-Allow-Origin": "*"
