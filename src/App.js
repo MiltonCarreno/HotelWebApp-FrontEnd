d54438d16f8795f089fb8df4c72367c0cd0649e1
MiltonCarreno <miltonyaredcr@gmail.com>
2023-03-06 22:55:43 -0500

    Added 'Header' to 'App'

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
-import Layout from './components/Layout';
 import {Routes, Route} from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from "./components/home/Home";
+import Header from "./components/header/Header";
 
 function App() {
   const [movies, setMovies] = useState();
@@ -10,7 +12,6 @@ function App() {
   const getMovies = async () =>{
     try {
       const response = await api.get("/api/v1/movies");
-      console.log(response.data)
       setMovies(response.data);
     } catch(err) {
       console.log(err)
@@ -22,7 +23,12 @@ function App() {
     }, [])
   return (
     <div className="App">
-      
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+        <Route path="/" element={<Home movies={movies}/>}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
