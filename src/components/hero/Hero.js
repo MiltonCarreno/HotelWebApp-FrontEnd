d8e09e5773b692a57245e4a3320846c40f1457a9
MiltonCarreno <miltonyaredcr@gmail.com>
2023-03-07 23:13:06 -0500

    Added fully-functional trailer button

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,6 +2,9 @@ import './Hero.css';
 import Carousel from 'react-material-ui-carousel';
 import {Paper} from '@mui/material';
 import { v4 } from 'uuid';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from 'react-router-dom';
 
 const Hero = ({movies}) => {
     return (
@@ -19,6 +22,13 @@ const Hero = ({movies}) => {
                                             </div>
                                             <div className='movie-title' key={v4()}>
                                                 <h4 key={v4()}>{movie.title}</h4>
+                                            </div>
+                                            <div className='movie-buttons-container'>
+                                                <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
+                                                    <div className='play-button-icon-container'>
+                                                        <FontAwesomeIcon className='play-button-icon' icon={faCirclePlay}/>
+                                                    </div>
+                                                </Link>
                                             </div>
                                         </div>
                                     </div>
